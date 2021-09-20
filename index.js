8e09026f9306d093eb8188d696d8c0e0bdbab1ec
AurionHoward <aurionhoward@gmail.com>
2021-09-20 19:08:14 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+let ride = 2001;
+function scuberGreetingForFeet(ride){
+ if (ride <= 400)
+ return 'This one is on me!'
+ if ( ride > 2000 && ride <= 2500)
+ return 'I will gladly take your thirty bucks.'
+ if (ride > 2500 )
+ return 'No can do.'
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+let city = 'Pitssburg';
+function ternaryCheckCity(city){
+  if ( city === 'NYC') return 'Ok, sounds good.'
+  if ( city != 'NYC') return 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+let tip = 'generous';
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    case 'thanks for everything':
+      return 'Bye.';
+  
+  }
 }
+
+
+ 
+
