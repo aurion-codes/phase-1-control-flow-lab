f89076a5a9ace4b4b45abc79b02bbbee7d133d17
AurionHoward <aurionhoward@gmail.com>
2021-09-24 23:43:02 -0400

    commit tree 32f9bc6d3b45348ef2b6d24365f5c3b05fe589f4

--- index.js
+++ index.js
@@ -1,36 +1,9 @@
 
-let ride = 2001;
-function scuberGreetingForFeet(ride){
- if (ride <= 400)
- return 'This one is on me!'
- if ( ride > 2000 && ride <= 2500)
- return 'I will gladly take your thirty bucks.'
- if (ride > 2500 )
- return 'No can do.'
+function scuberGreetingForFeet(){
+  if (ride <= 400) {
+    return 'This one is on me!';
+  } else if ( ride > 2000 ){
+    return 'I will gladly take your thirty bucks.';
+  } 
 
 }
-
-
-
-let city = 'Pitssburg';
-function ternaryCheckCity(city){
-  if ( city === 'NYC') return 'Ok, sounds good.'
-  if ( city != 'NYC') return 'No go.'
-}
-
-let tip = 'generous';
-function switchOnCharmFromTip(tip){
-  switch(tip){
-    case 'generous':
-      return 'Thank you so much.'
-    case 'not as generous':
-      return 'Thank you.'
-    case 'thanks for everything':
-      return 'Bye.';
-  
-  }
-}
-
-
- 
-
